e06dd4ae2fd8027a2f230e520dd23f5a3c1b11d0
keishaharris <keishadharriss@gmail.com>
2023-04-22 07:09:02 -0400

    static coding of Header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,28 @@
 const Home = () => {
     return (
-        <>
-        <h1 className="text-3xl font-bold underline">
-        AND THIS IS MY HOME PAGE
-        </h1>
-        </>
+        <main className="w-screen h-screen flex justify-center items-center gap-6">
+            
+                
+            <div className="profile-img">
+            </div>
+
+            <div>
+                <h1 className="text-4xl font-bold">
+                    Keisha Harris _
+                </h1>
+
+                <span>const iAm: software engineer.</span>
+                <span>const location: New York Based </span> <br />
+                <span>Developer focused on creating practical solutions</span>
+                <div className="flex">
+                    <div className="peach square2"></div>
+                    <div className="orange square2"></div>
+                    <div className="yellow square2"></div>
+                    <div className="forest square2"></div>
+                
+                </div>
+            </div>
+        </main>
     )
 }
 export default Home;
